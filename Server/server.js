35cb3f3111286027d292f3c80d2f60d85195cdc9
sigalavera <sigi1697@gmail.com>
2021-08-09 17:00:39 +0300

    commit

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 const cors = require('cors');
 const router = require('./routers/studentRouters')
 const db = require('./DB/Index')
+const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json())
@@ -12,13 +13,22 @@ db.on('error',()=>{console.log("fgfgfghfhgf");})
 
 app.use('/student', router);
 
-app.get('/', (req, res)=>{
-  res.send("gooooooood")
-})
+// app.get('/', (req, res)=>{
+//   res.send("gooooooood")
+// })
 
 app.listen(27017, (error) => {
   if (error) return "error"
   console.log("The Server is on air, in port:27017");
 });
 
-
+//*************************/
+if(process.env.NODE_ENV === 'production'){
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing 
+  app.get('*',(req,res)=>{
+      res.sendFile(path.join(__dirname, '../client/build','index.html'))
+  })
+}
+//*************************/
